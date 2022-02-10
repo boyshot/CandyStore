dddffb8be18c98c97a61fc9beec6b2478d723326
Paulo Rocha <pauloandredarocha@gmail.com>
2022-02-10 11:10:42 -0300

    Implementação do docker compose, BD mysql e Implementação da Api de Produtos

---
+++ backend/src/CandyStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using CandyStore.Application.Product;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CandyStore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> Get()
+        {
+            return null;            
+        }
+
+
+
+    }
+}
